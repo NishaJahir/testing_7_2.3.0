582919fcfad07ebccc86d2c212cdd145e37a9f79
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-05-31 18:16:57 +0530

    Update CreateNewPaymentMethods.php

--- src/Migrations/CreateNewPaymentMethods.php
+++ src/Migrations/CreateNewPaymentMethods.php
@@ -24,7 +24,7 @@ use Novalnet\Helper\PaymentHelper;
  *
  * @package Novalnet\Migrations
  */
-class CreateNewPaymentMethods
+class CreateNewPaymentMethods123
 {
     /**
      * @var PaymentMethodRepositoryContract
@@ -56,22 +56,23 @@ class CreateNewPaymentMethods
      */
     public function run()
     {
-        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_CC', 'Novalnet Credit/Debit Cards');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_SEPA', 'Novalnet Direct Debit SEPA');
+        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_CC', 'Novalnet Credit/Debit Cards');
+        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_APPLEPAY', 'Novalnet ApplePay');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_INVOICE', 'Novalnet Invoice');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_PREPAYMENT', 'Novalnet Prepayment');
-        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_CASHPAYMENT', 'Novalnet Barzahlen/viacash');
-        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_SOFORT', 'Novalnet Sofort');
-        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_PAYPAL', 'Novalnet PayPal');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_IDEAL', 'Novalnet iDEAL');
-        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_EPS', 'Novalnet eps');
+        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_SOFORT', 'Novalnet Sofort');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_GIROPAY', 'Novalnet giropay');
+        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_CASHPAYMENT', 'Novalnet Barzahlen/viacash');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_PRZELEWY', 'Novalnet Przelewy24');
-        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_APPLEPAY', 'Novalnet ApplePay');
+        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_EPS', 'Novalnet eps');
+        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_PAYPAL', 'Novalnet PayPal');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_POSTFINANCE_CARD', 'Novalnet PostFinance Card');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_POSTFINANCE_EFINANCE', 'Novalnet PostFinance E-Finance');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_BANCONTACT', 'Novalnet Bancontact');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_MULTIBANCO', 'Novalnet Multibanco');
+        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_ONLINE_BANK_TRANSFER', 'Novalnet Online bank transfer');
     }
 
 
